5f47e8cc93fd6b86d87264c30f87569fe01ae742
Sandro Brändli <sandro.braendli@gmail.com>
2018-10-17 22:51:06 +0200

    Show empty field

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,38 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QGridLayout
+from copy import deepcopy
 
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec_()
+
+BOARD_SIZE = 3
+REQUIRED_FOR_WINNING = 3
+
+
+class Board:
+
+    def __init__(self, board_size, required_for_winning, other=None):
+        self.required_for_winning = required_for_winning
+        self.board_size = board_size
+
+        if other:
+            self.__dict__ = deepcopy(other.__dict__)
+
+
+class Gui:
+
+    def __init__(self, board):
+        self.board = board
+
+    def show(self):
+        app = QApplication([])
+        window = QWidget()
+        layout = QGridLayout(window)
+
+        for x in range(self.board.board_size):
+            for y in range(self.board.board_size):
+                layout.addWidget(QPushButton(), x, y)
+
+        window.show()
+        app.exec_()
+
+
+if __name__ == '__main__':
+    Gui(Board(BOARD_SIZE, REQUIRED_FOR_WINNING)).show()
