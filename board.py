3a551939a708485fd3a2ea22d141f2241a88a957
Sandro Brändli <sandro.braendli@gmail.com>
2018-10-24 13:43:50 +0200

    Implement transposition table and choose a move even if there are only losing ones

--- board.py
+++ board.py
@@ -1,7 +1,9 @@
 from typing import List, Tuple, Any
 
+PRIME = (2 ** 19 - 1)
 
 WON: int = 100000
+INF: int = 9 * WON
 
 
 class Board:
@@ -13,6 +15,8 @@ class Board:
     fields: List[List[str]]
     is_first_player: bool
     past_moves: List[Tuple[int, int]]
+    num_evaluations: int
+    transposition_table: List
 
     def __init__(self, first_player: str, second_player: str, board_size: int = 3, required_for_winning: int = 3):
         self.first_player = first_player
@@ -22,6 +26,7 @@ class Board:
         self.fields = [['' for _ in range(self.board_size + 1)] for _ in range(self.board_size + 1)]
         self.is_first_player = True
         self.past_moves = []
+        self.transposition_table = [None for _ in range(PRIME)]
 
     def move(self, move: Tuple[int, int]) -> None:
         self.fields[move[0]][move[1]] = self.first_player if self.is_first_player else self.second_player
@@ -37,6 +42,7 @@ class Board:
         self.is_first_player = not self.is_first_player
 
     def score(self) -> int:
+        self.num_evaluations += 1
         if not self.past_moves:
             return 0
 
@@ -63,19 +69,37 @@ class Board:
         return count
 
     def cpu_move(self):
-        move = negamax(self, -WON, WON)[1]
+        self.num_evaluations = 0
+        move = negamax(self, -INF, INF)[1]
         if not move is None:
             self.move(move)
+        print(self.num_evaluations)
+        print(self.transposition_table[self.get_hash()])
+
+    def get_hash(self):
+        hash_value = 0
+
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                field = self.fields[x][y]
+                hash_value = 3 * hash_value + (1 if field == self.first_player else 2 if field == self.second_player else 0)
+
+        return hash_value % PRIME
 
 
 def negamax(node, alpha, beta) -> Tuple[int, Any]:
     possible_moves: List = node.possible_moves()
+    entry = node.transposition_table[node.get_hash()]
+
+    if not entry is None:
+        return entry
+
     score = node.score()
 
     if not possible_moves or score == WON:
         return -score, None
 
-    best = -WON, None
+    best = -INF, None
     for move in possible_moves:
         node.move(move)
         value = -negamax(node, -beta, -alpha)[0]
@@ -89,4 +113,5 @@ def negamax(node, alpha, beta) -> Tuple[int, Any]:
         if alpha >= beta:
             break
 
+    node.transposition_table[node.get_hash()] = best
     return best
