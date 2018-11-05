f6079f53ad7a502c03a16f62518cd274c60fd099
Laura Brändli <laurasophie007@gmail.com>
2018-11-05 20:45:06 +0100

    Let computer play first

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,4 +6,6 @@ BOARD_SIZE = 4
 REQUIRED_FOR_WINNING = 3
 
 if __name__ == '__main__':
-    Gui(Board('X', 'O', BOARD_SIZE, REQUIRED_FOR_WINNING)).show()
+    b = Board('X', 'O', BOARD_SIZE, REQUIRED_FOR_WINNING)
+    b.cpu_move()
+    Gui(b).show()
